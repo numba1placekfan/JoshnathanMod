05a4896f0cd5a0fb007ed88a61b3d1dc39f523d4
joe <87423906+numba1placekfan@users.noreply.github.com>
2024-02-18 21:31:32 +0100

    it now has a name and allat
    next up: ill make it throwable

--- src/main/java/com/john/gemerald/Items/ModItems.java
+++ src/main/java/com/john/gemerald/Items/ModItems.java
@@ -1,5 +1,6 @@
 package com.john.gemerald.Items;
 
+import com.john.gemerald.Items.special.CdDvd;
 import com.john.gemerald.Items.special.Soyringe;
 import com.john.gemerald.gemerald;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -25,6 +26,7 @@ public class ModItems
         tangerinerald = new ItemFood(20,1.0F, false).setUnlocalizedName("tangerinerald").setCreativeTab(CreativeTabs.tabFood).setTextureName(gemerald.MODID + ":tangerinerald");
         josh_ingot = new Item().setUnlocalizedName("josh_ingot").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(gemerald.MODID + ":josh_ingot");
         soyringe = new Soyringe();
+        cd = new CdDvd();
 
         GameRegistry.registerItem(tangerinerald, tangerinerald.getUnlocalizedName());
         GameRegistry.registerItem(coal_gem, coal_gem.getUnlocalizedName());
